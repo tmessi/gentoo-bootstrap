0ff9358f5b825443a090fecd1788653e0514df87
Timothy Messier <tim.messier@gmail.com>
2013-11-07 16:39:58 -0500

    Removed un-needed seperate scripts

--- 02-cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /etc/profile
-
-# install cron
-chroot "$chroot" /bin/bash <<DATAEOF
-emerge sys-process/fcron
-rc-update add fcron default
-DATAEOF
--- 05-salt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /etc/profile
-
-# Install salt via bootstrap
-chroot "$chroot" /bin/bash <<DATAEOF
-wget -O - http://bootstrap.saltstack.org | sh
-DATAEOF
-
